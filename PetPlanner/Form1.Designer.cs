712fe3574b9a7c60e72e3caf987ee94d6ae76181
unknown <mezeus2015@yandex.ru>
2019-04-30 20:41:13 +0500

    Необходимо доделать занос в массив и чтение из текстбоксов

--- PetPlanner/Form1.Designer.cs
+++ PetPlanner/Form1.Designer.cs
@@ -28,13 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(476, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Отправить данные";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(71, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(331, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(546, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 39);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Открыть файл";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(408, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(132, 39);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Добавить Задачу";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(690, 223);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PetPlanner :)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- PetPlanner/Form1.cs
+++ PetPlanner/Form1.cs
@@ -7,14 +7,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
 
 namespace PetPlanner
 {
     public partial class Form1 : Form
     {
+        public int newTextBoxPosition = 12;
+        public int labelCount = 1;
+        public int mainCount = 0;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e) //Кнопка записи в файл из текстбокса
+        {
+            /* 
+            * Всё вместить в массив и сделать вывод
+            */
+            string lines = textBox1.Text + "\n"; 
+            File.WriteAllText(@"E:\Projects\PetPlanner\SaveHere\text.txt", lines);
+            textBox1.Text = "";
+            MessageBox.Show("Сохранение прошло успешно!");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) //Кнопка открытия файла из текстбокса
+        {
+            Process.Start(@"E:\Projects\PetPlanner\SaveHere\text.txt");
+        }
+
+        private void button3_Click(object sender, EventArgs e) //Кнопка добавления задачи 
+        {
+            /* 
+            * Добавляем поле TextBox
+            */
+            newTextBoxPosition += 26;
+            string textBoxName = "textBox" + mainCount;
+            this.Controls.Add(new TextBox() { Name = textBoxName, Location = new Point(71, newTextBoxPosition), Text = "", Size = new Size(331, 20) });
+
+            /* 
+            * Добавляем Label
+            */
+
+            labelCount += 1;
+
+            string labelName = "label" + mainCount;
+            this.Controls.Add(new Label() { Name = labelName, Location = new Point(52, newTextBoxPosition + 3), Text = labelCount.ToString() });
+            
+
+            mainCount += 1;
+        }
     }
+
 }
+
